a8caa40fec0015956ab7fa06989baf6f8c90be3e
pavan-6860 <pavan-6860@pavan-6860.csez.zohocorpin.com>
2018-05-26 10:57:59 +0530

    base commit

---
+++ app/src/main/java/com/example/pavan_6860/wakeupindia/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.pavan_6860.wakeupindia;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
