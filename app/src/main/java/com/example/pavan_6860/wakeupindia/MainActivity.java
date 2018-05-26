51fe99326fe41aafa5b4e818003fceec8e694361
pavan-6860 <mouse.pavan@gmail.com>
2018-05-26 12:40:01 +0530

    basic layout is done with testing need to add floating edit text and need to validate the input

--- app/src/main/java/com/example/pavan_6860/wakeupindia/MainActivity.java
+++ app/src/main/java/com/example/pavan_6860/wakeupindia/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.pavan_6860.wakeupindia;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void signIn(View view){
+        Intent homeIntent=new Intent(this,Home.class);
+        try{
+            startActivity(homeIntent);
+        }
+        catch (Exception e){
+            Toast.makeText(this,getResources().getString(R.string.error_message),Toast.LENGTH_LONG).show();
+        }
     }
 }
